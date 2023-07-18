e2b30151380ca5a6840a649a4e0c7c502cf83478
Iftach Naftaly <iftahnaf@gmail.com>
2023-07-18 14:12:40 +0000

    automatic compile the project

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 echo "Updating submodule ..."
 git submodule update --init --recursive
+
+echo "Installing dependencies ..."
+pip install rospkg
+sudo apt update
+sudo apt install -y python-is-python3
+
+sudo rm -rf /etc/ros/rosdep/sources.list.d/20-default.list 
+sudo rosdep init
+rosdep update
+
 echo "Compiling the project ..."
+source /opt/ros/noetic/setup.bash
+export ROS_PACKAGE_PATH=$(pwd):$ROS_PACKAGE_PATH
 make
